8ae492b4abfc2df94d8ccf4266a964304e1c3ba9
arior <arior@indra.es>
2025-03-19 13:01:10 +0100

    Se añade la clase Empleado.java inicial

---
+++ Empleado.java
@@ -0,0 +1,25 @@
+public class Empleado {
+    private String nombre;
+    private int antiguedad;
+    public Empleado(String nombre, int antiguedad) {
+        this.nombre = nombre;
+        this.antiguedad = antiguedad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+    public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+
+    
+
+
+
+}
